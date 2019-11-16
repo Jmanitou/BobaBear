74dac5b2c5097ab361e715f6b5937bc75bc3c672
Jackson Robbins (RIT Student) <jsr5501@ad.rit.edu>
2019-11-15 22:00:12 -0500

    Zoom, UnZoom, Pickup started

--- BobaBear/Assets/Scripts/Object.cs
+++ BobaBear/Assets/Scripts/Object.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Object : MonoBehaviour
 {
+    public Vector2 position;
+    public float scaleX;
+    public float scaleY;
+    public float scaleZ;
     public bool pickupable;
     public bool pickedUp;
+    Player player = GameObject.Find("Player").GetComponent<Player>();
 
     // Start is called before the first frame update
     void Start()
     {
+        gameObject.SetActive(true);
         pickedUp = false;
+        position = gameObject.transform.position;
+        scaleX = gameObject.transform.localScale.x;
+        scaleY = gameObject.transform.localScale.y;
+        scaleZ = gameObject.transform.localScale.z;
     }
 
     // Update is called once per frame
@@ -21,11 +31,21 @@ public class Object : MonoBehaviour
 
     void Zoom()
     {
+        gameObject.transform.position = new Vector3(0,0,0);
+        //gameObject.transform.localScale = new Vector3(5,5,0); change later
+    }
 
+    void UnZoom()
+    {
+        gameObject.transform.position = position;
+        gameObject.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
     }
 
     void PickUp()
     {
+        player.inventory.Add(this);
+        GetComponent<MeshRenderer>().enabled = false;
+        gameObject.SetActive(false);
 
     }
 }
