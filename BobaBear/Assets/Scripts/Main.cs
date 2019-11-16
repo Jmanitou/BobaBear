143e46c97a85cd003528a3839da1441b50c448ac
Gavin <gmd3659@rit.edu>
2019-11-15 21:50:46 -0500

    Main
    Main class architecture written but may or may not be used

--- BobaBear/Assets/Scripts/Main.cs
+++ BobaBear/Assets/Scripts/Main.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    string state;
+    string room;
+    string zoomed;
+    string win;
+    string lose;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Create string for each state
+        room = "room";
+        zoomed = "zoomed";
+        win = "win";
+        lose = "lose";
+
+        //Starting state is menu
+        state = room;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Change based on state
+        switch (state)
+        {
+            case "room":
+                break;
+
+            case "zoomed":
+                break;
+
+            case "win":
+                break;
+
+            case "lose":
+                break;
+
+            default:
+                break;
+        }
     }
 }
