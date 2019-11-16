3983c587c78a69ba2c66a7943236424fa3b53ea0
JustChengBoy <37488838+JustChengBoy@users.noreply.github.com>
2019-11-15 21:35:28 -0500

    asdasda

--- BobaBear/Assets/Scripts/Player.cs
+++ BobaBear/Assets/Scripts/Player.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Vector3 target;
+    private List<Object> inventory;
+    private GameObject camera;
+    public float camSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = GetComponent<GameObject>();
+        camSpeed = 5.0f;
+        inventory = new List<Object>();
     }
 
     // Update is called once per frame
