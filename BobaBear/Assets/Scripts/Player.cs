ef0e8f2b2036ad8a02f08557da345a53997335f5
JustChengBoy <37488838+JustChengBoy@users.noreply.github.com>
2019-11-15 22:27:57 -0500

    Fixed the camera script pan

--- BobaBear/Assets/Scripts/Player.cs
+++ BobaBear/Assets/Scripts/Player.cs
@@ -4,38 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Vector3 target;
     public List<Object> inventory;
     public Camera cam;
     public float camSpeed;
-    public float bounds;
+    public float leftLimit;
+    public float rightLimit;
 
     // Start is called before the first frame update
     void Start()
     {
-        cam = Camera.main;
-        camSpeed = 5f;
         inventory = new List<Object>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-        //Panning the camera left and right only
+        CameraMove();
+    }
+    
+    //Panning the camera left and right only
+    void CameraMove()
+    {
         Vector3 pos = cam.transform.position;
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            pos.x -= camSpeed * Time.deltaTime;
+            if (pos.x > leftLimit)
+            {
+                pos.x -= camSpeed * Time.deltaTime;
+            }
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            pos.x += camSpeed * Time.deltaTime;
+            if (pos.x < rightLimit)
+            {
+                pos.x += camSpeed * Time.deltaTime;
+            }
         }
-
-        pos.x = Mathf.Clamp(pos.x, bounds, bounds);
         cam.transform.position = pos;
     }
 }
