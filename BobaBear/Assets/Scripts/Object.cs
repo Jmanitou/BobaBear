a509552c8f6ff07d49349f9c68597bdd90363c58
Jackson Robbins (RIT Student) <jsr5501@ad.rit.edu>
2019-11-15 21:26:39 -0500

    Object
    Object class architecture

--- BobaBear/Assets/Scripts/Object.cs
+++ BobaBear/Assets/Scripts/Object.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Object : MonoBehaviour
 {
+    public bool pickupable;
+    public bool pickedUp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pickedUp = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Zoom()
+    {
+
+    }
+
+    void PickUp()
+    {
+
     }
 }
